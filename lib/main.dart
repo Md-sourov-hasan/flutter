26a5f383152a5b13e346bef0f04dde2f13624ed0
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-10 14:29:29 +0600

    section 2 video 5 half

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,21 @@ import 'package:flutter/material.dart';
 import 'package:first_app/start_screen.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(gradient: LinearGradient(
+            colors:[
+                const Color.fromARGB(255, 45, 20, 158),
+               const Color.fromARGB(255, 97, 68, 148)
+            ],
+            begin:Alignment.topLeft,
+            end:Alignment.bottomRight,
+          ),),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
