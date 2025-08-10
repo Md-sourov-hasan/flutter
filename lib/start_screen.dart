5bed91191067ac0c32cdf2e06c885832ec8e8ce4
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-10 18:54:23 +0600

    section 2 video 56 finished

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
-class StartScreen extends StatelessWidget{
+class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
   Widget build(context) {
-    return Center(child: const Text("Start Screen"));
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [Image.asset('assets/image/question.png', width: 300,),
+       const SizedBox(height: 80),
+      const Text("start quiz", style: TextStyle(
+        color: Colors.white,
+        fontSize:24,
+      )),
+     const SizedBox(height: 30,),
+     OutlinedButton(onPressed: () {}, 
+     style: OutlinedButton.styleFrom(
+      foregroundColor: Colors.white,
+     ),
+     child:const Text("start QUIZ"),)
+     
+        ],
+      ),
+    );
   }
-
 }
