0778b439b02717e52b88baece95035ee0effcf03
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-11 07:54:11 +0600

    section 3 video 58

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -8,18 +8,19 @@ class StartScreen extends StatelessWidget {
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
-        children: [Image.asset('assets/image/question.png', width: 300,),
+        children: [Image.asset('assets/image/question.png', width: 300,color:const Color.fromARGB(255, 238, 236, 236),),
        const SizedBox(height: 80),
       const Text("start quiz", style: TextStyle(
         color: Colors.white,
         fontSize:24,
       )),
      const SizedBox(height: 30,),
-     OutlinedButton(onPressed: () {}, 
+     OutlinedButton.icon(onPressed: () {}, 
      style: OutlinedButton.styleFrom(
       foregroundColor: Colors.white,
      ),
-     child:const Text("start QUIZ"),)
+     icon: Icon(Icons.arrow_right_alt),
+     label:const Text("start QUIZ"),)
      
         ],
       ),
