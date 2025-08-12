3e111ac0b2ff1d6cd22801a07af05ca9127869da
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-12 07:51:41 +0600

    first interface half

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
-
 import 'package:first_app/start_screen.dart';
-
-void main() {
+void main(){
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(gradient: LinearGradient(
-            colors:[
+MaterialApp(
+  home: Scaffold(
+    body: Container( 
+      decoration:BoxDecoration( 
+       gradient:  LinearGradient( 
+          colors:[
                 const Color.fromARGB(255, 45, 20, 158),
                const Color.fromARGB(255, 97, 68, 148)
             ],
-            begin:Alignment.topLeft,
-            end:Alignment.bottomRight,
-          ),),
-          child: const StartScreen(),
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
         ),
+
       ),
+      child: const StartScreen( ),
+
     ),
+  )
+)
   );
 }
