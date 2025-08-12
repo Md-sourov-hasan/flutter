42f5a2e4196a92fd58ebb9dee97e9c7fecc70002
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-12 09:47:07 +0600

    section 3 video 59

---
+++ lib/quize.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/start_screen.dart';
+class Quiz extends StatefulWidget{
+  const Quiz({super.key});
+  @override
+  State <Quiz> createState() {
+return _QuizState();
+  }
+}
+class _QuizState extends State<Quiz>{
+  @override
+  Widget build(context){ 
+    return MaterialApp(
+  home: Scaffold(
+    body: Container( 
+      decoration:BoxDecoration( 
+       gradient:  LinearGradient( 
+          colors:[
+                const Color.fromARGB(255, 45, 20, 158),
+               const Color.fromARGB(255, 97, 68, 148)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+        ),
+
+      ),
+      child: const StartScreen( ),
+
+    ),
+  )
+);
+  }
+}
