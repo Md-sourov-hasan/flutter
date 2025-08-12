4c3543fa3e35c20e9968442a4ac9abc5ddefe4ec
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-12 08:35:11 +0600

    first interface finished

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-
-class StartScreen extends StatelessWidget {
+class StartScreen extends StatelessWidget{
   const StartScreen({super.key});
+@override
+Widget build(context){
+  return Center(
+child:Column(
+  mainAxisSize:MainAxisSize.min,
+  children:[Image.asset("assets/image/question.png", width: 300,color:const Color.fromARGB(255, 238, 236, 236),),
+  const SizedBox(height:80),
+  const Text("start quiz",style: TextStyle(
+    color:Colors.white,
+    fontSize:24,
+  )),
+  const SizedBox(height:30,),
+  OutlinedButton.icon(onPressed:() {},
+  style:OutlinedButton.styleFrom(
+    foregroundColor:Colors.white,
+  ),
+  icon:Icon(Icons.arrow_right_alt),
+  label:const Text("start quiz"),
 
-  @override
-  Widget build(context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [Image.asset('assets/image/question.png', width: 300,color:const Color.fromARGB(255, 238, 236, 236),),
-       const SizedBox(height: 80),
-      const Text("start quiz", style: TextStyle(
-        color: Colors.white,
-        fontSize:24,
-      )),
-     const SizedBox(height: 30,),
-     OutlinedButton.icon(onPressed: () {}, 
-     style: OutlinedButton.styleFrom(
-      foregroundColor: Colors.white,
-     ),
-     icon: Icon(Icons.arrow_right_alt),
-     label:const Text("start QUIZ"),)
-     
-        ],
-      ),
-    );
-  }
+  )
+  ],
+
+),
+  );
+} 
 }
