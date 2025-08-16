6aeac1ee29ad3a28a2476305ca751b11d63efcb7
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-16 08:35:48 +0600

    section 3 video 73

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -18,24 +18,30 @@ final currentQuestion=questions[0];
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-       mainAxisAlignment: MainAxisAlignment.center, 
-        children: [
-
-
-        Text(currentQuestion.text, style: const TextStyle(
-        color:Colors.white,
-       ) ,),
-
-
-        const SizedBox(height: 30,),
-
-       
-        ...currentQuestion.answers.map((answer) {
-          return AnswerButton(answerText: answer, onTap: () {},);
-        })
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+         mainAxisAlignment: MainAxisAlignment.center, 
+         crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
         
-      ],),
+        
+          Text(currentQuestion.text, style: const TextStyle(
+          color:Colors.white,
+         ),
+         textAlign: TextAlign.center,
+         ),
+        
+        
+          const SizedBox(height: 30,),
+        
+         
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {},);
+          })
+          
+        ],),
+      ),
     );
   }
 }
