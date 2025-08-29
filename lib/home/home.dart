4de6f3931b076a31decf5fe012caf36c8a124aca
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-29 09:01:59 +0600

    ggg

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -4,26 +4,40 @@ class HomeView extends StatelessWidget {
 
 @override
   Widget build(BuildContext context) {
-    return Scaffold(
-appBar: AppBar(
-  backgroundColor: Color(0xff008268),
-  centerTitle: false,
-
-  title:const Text("whatsapp",
-style: TextStyle(
-  color: Colors.white,
-  fontWeight: FontWeight.w600,
-),
- ),
-actions: [
-  IconButton(onPressed: () {}, icon : Icon(Icons.camera_alt_outlined, color : Colors.white)),
-  IconButton(onPressed: () {}, icon : Icon(Icons.search, color : Colors.white)),
-  IconButton(onPressed: () {}, icon : Icon(Icons.more_vert, color : Colors.white))
-],
-
-),
-
-
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff008268),
+        centerTitle: false,
+      
+        title:const Text("whatsapp",
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.w600,
+      ),
+       ),
+      actions: [
+        IconButton(onPressed: () {}, icon : Icon(Icons.camera_alt_outlined, color : Colors.white)),
+        IconButton(onPressed: () {}, icon : Icon(Icons.search, color : Colors.white)),
+        IconButton(onPressed: () {}, icon : Icon(Icons.more_vert, color : Colors.white)),
+      
+      ],
+      bottom: TabBar(
+        tabs: [
+      Tab(
+        text: "chats",
+      ) ,
+        Tab(
+        text: "updates",
+      ) 
+        ],
+      ),
+      
+      ),
+      
+      
+      ),
     );
   }
 
