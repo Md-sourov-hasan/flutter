714b54d5b12c725352c62cfe1cd6751ee39f3cf9
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-29 15:30:35 +0600

    mmmmm

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -70,6 +70,44 @@ class HomeView extends StatelessWidget {
         leading: CircleAvatar(
           backgroundImage: NetworkImage('https://static.wikia.nocookie.net/naruto/images/4/4c/Shisui_Uchiha.png/revision/latest?cb=20140418091747'),
         ),
+        title: Text("md sourov",
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        ),
+        subtitle: Text('how are yoy',
+        style: TextStyle(
+          color: const Color.fromARGB(255, 82, 79, 79),
+        ),
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Text('4 : 40 pm',
+            style: TextStyle(
+              color:  Color(0xff008268),
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            Container(
+              height: 25,
+              width: 25,
+              decoration: BoxDecoration(
+                color:  Color(0xff008268),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Text('2',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
 
       ],
