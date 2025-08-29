d1a520b800c0dbb325d516d2e3db288cacea7e24
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-30 00:00:07 +0600

    njnb

---
+++ lib/home/tab_views/calls_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CallsTab extends StatelessWidget {
+const CallsTab({super.key});
+
+@override
+  Widget build(BuildContext context) {
+   return const Center(
+    child: Text('calls_tab'),
+   );
+  }
+
+
+}
---
+++ lib/home/tab_views/updates_tab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UpdateTab extends StatelessWidget {
+const UpdateTab({super.key});
+
+@override
+  Widget build(BuildContext context) {
+   return const Center(
+    child: Text('update'),
+   );
+  }
+
+
+}
