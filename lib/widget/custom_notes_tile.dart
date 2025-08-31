a2a44e7421e1b83dd5e3854b8df2e4bff994e8c1
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-31 20:59:21 +0600

    link custom widget 2 step

--- lib/widget/custom_notes_tile.dart
+++ lib/widget/custom_notes_tile.dart
@@ -7,10 +7,23 @@ class CustomNotesTile extends StatelessWidget{
   Widget build(BuildContext context) {
   return Container(
             height: 70,
+            padding: EdgeInsets.all(15),
             decoration: BoxDecoration(
-              color: Color(0xFFf5f5f5f5f5f5f5),
+              color:const Color(0xFFf5f5f5f5f5f5f5),
               borderRadius: BorderRadius.circular(11),
             ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Title Here',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 16,
+                  ),
+                  )
+                ],
+              ),
           );
   }
 }
