a194c5b2686b64b1f64e9a5f37815b1abc9955bf
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-31 22:16:02 +0600

    add a leading bitton back arrow

--- lib/home/notes/create_or_update.dart
+++ lib/home/notes/create_or_update.dart
@@ -5,6 +5,20 @@ class CreateOrUpdateNoteView extends StatelessWidget{
   const CreateOrUpdateNoteView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFf5f5f5f5f5f5f5),
+        automaticallyImplyLeading: false,
+        leading: IconButton(onPressed: () {
+
+          
+        }, 
+        
+        icon: Icon(
+          Icons.arrow_back
+
+        )),
+      ),
+    );
   }
 }
