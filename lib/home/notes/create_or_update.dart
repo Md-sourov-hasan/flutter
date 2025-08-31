9e13448540dc2efb1fa773a706d443d415c8dd48
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-31 22:30:28 +0600

    add a done button

--- lib/home/notes/create_or_update.dart
+++ lib/home/notes/create_or_update.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/instance_manager.dart';
 
 class CreateOrUpdateNoteView extends StatelessWidget{
   const CreateOrUpdateNoteView({super.key});
@@ -10,15 +12,22 @@ class CreateOrUpdateNoteView extends StatelessWidget{
         backgroundColor: const Color(0xFFf5f5f5f5f5f5f5),
         automaticallyImplyLeading: false,
         leading: IconButton(onPressed: () {
+          Get.back();
 
-          
         }, 
         
         icon: Icon(
           Icons.arrow_back
 
         )),
+        title: Text('create note'),
+        centerTitle: false,
+        actions: [
+          TextButton(onPressed: () {}, child: const Icon(Icons.done)
+          ),
+        ],
       ),
+      
     );
   }
 }
