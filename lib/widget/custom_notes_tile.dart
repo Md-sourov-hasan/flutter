522558c1b7a795593f2261913d50b023ff74e580
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-31 21:08:47 +0600

    add a discription

--- lib/widget/custom_notes_tile.dart
+++ lib/widget/custom_notes_tile.dart
@@ -6,7 +6,7 @@ class CustomNotesTile extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
   return Container(
-            height: 70,
+            // height: 70,
             padding: EdgeInsets.all(15),
             decoration: BoxDecoration(
               color:const Color(0xFFf5f5f5f5f5f5f5),
@@ -21,7 +21,10 @@ class CustomNotesTile extends StatelessWidget{
                     fontWeight: FontWeight.w700,
                     fontSize: 16,
                   ),
-                  )
+                  ),
+                 Text('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.',
+                 maxLines: 1,
+                 ),
                 ],
               ),
           );
