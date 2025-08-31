f99bcce1fb7f6213cd7882f5a44fb03fbdef2cb1
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-08-31 20:17:14 +0600

    note app body first step

--- lib/home/notes/notes.dart
+++ lib/home/notes/notes.dart
@@ -7,15 +7,29 @@ const NotesView ({super.key});
 @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFf5f5f5f5f5f5f5),
       appBar: AppBar(
-        backgroundColor: Colors.grey,
+        backgroundColor: Color(0xFFf5f5f5f5f5f5f5),
         centerTitle: false,
         title: Text('Notes'),
         actions: [
           IconButton(onPressed: () {}, icon: Icon(Icons.settings)),
         ],
       ),
-      body: ,
+      body: ListView.separated(
+        
+        itemBuilder: (context, index) {
+          return Text('fgfg')
+        },
+       separatorBuilder: (context,index){
+        return const SizedBox(
+          height: 10,
+
+        );
+       },
+        itemCount: 5,
+        
+        ),
 
     );
   }
