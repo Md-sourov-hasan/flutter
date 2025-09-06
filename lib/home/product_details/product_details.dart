2278ddf6c08d99b346684a0e51e249cc851816e8
Md-sourov-hasan <mdsourovhasan@gmail.com>
2025-09-07 00:45:05 +0600

    hnjhb

--- lib/home/product_details/product_details.dart
+++ lib/home/product_details/product_details.dart
@@ -1,19 +1,18 @@
-
 import 'package:flutter/material.dart';
+import 'package:first_app/home/product_details/product_slider_section.dart';
 
 class ProductDetailsView extends StatelessWidget {
   const ProductDetailsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text('product details'),
+        title: const Text('Product Details'),
       ),
       body: Column(
-        children: [
-
+        children: const [
+          ProductSliderSection(), // সেমিকোলন বাদ দিয়ে কমা (,) ব্যবহার করা হয়েছে
         ],
       ),
     );
--- lib/home/product_details/product_slider_section.dart
+++ lib/home/product_details/product_slider_section.dart
@@ -1,39 +1,103 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class ProductSliderSec extends StatefulWidget {
-  const ProductSliderSec({super.key});
+class ProductSliderSection extends StatefulWidget {
+  const ProductSliderSection({super.key});
 
   @override
-  State<ProductSliderSec> createState() => _ProductSliderSecState();
+  State<ProductSliderSection> createState() => _ProductSliderSectionState();
 }
 
-class _ProductSliderSecState extends State<ProductSliderSec> {
+class _ProductSliderSectionState extends State<ProductSliderSection> {
   List<String> sliders = [
     'https://eu-images.contentstack.com/v3/assets/blt7dcd2cfbc90d45de/bltca0defe0e9c1b630/67b844be273493d43a833ed1/28513-1.jpg?format=pjpg&auto=webp&quality=75%2C90&width=3840',
     'https://zippo.co.za/cdn/shop/files/1OB36-03.png?v=1697700018',
     'https://www.thebeachcompany.in/cdn/shop/products/121123901_g1_800x.jpg?v=1673418204'
   ];
 
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      itemCount: sliders.length,
-      itemBuilder: (context, index, realIndex) {
-        return Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(sliders[index]),
-              fit: BoxFit.cover,
+    return Stack(
+      children: [
+        CarouselSlider.builder(
+          itemCount: sliders.length,
+          itemBuilder: (context, index, realIndex) {
+            return Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(sliders[index]),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            );
+          },
+          options: CarouselOptions(
+            height: 300,
+            viewportFraction: 1,
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+          ),
+        ),
+
+        // Dot Indicator Positioned
+        Positioned(
+          bottom: 16,
+          left: 0,
+          right: 0,
+          child: Center(
+            child: PageViewDotIndicator(
+              currentItem: currentIndex,
+              count: sliders.length,
+              unselectedColor: Colors.grey.withOpacity(0.6),
+              selectedColor: Colors.white,
             ),
           ),
-        );
-      },
-      options: CarouselOptions(
-        height: 300,
-        viewportFraction: 1,
-        autoPlay: false, // চাইলে auto slide করতে পারো
-      ),
+        ),
+      ],
     );
   }
 }
+
+/// Dot Indicator Widget (same file)
+class PageViewDotIndicator extends StatelessWidget {
+  final int currentItem;
+  final int count;
+  final Color selectedColor;
+  final Color unselectedColor;
+
+  const PageViewDotIndicator({
+    super.key,
+    required this.currentItem,
+    required this.count,
+    this.selectedColor = Colors.white,
+    this.unselectedColor = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(count, (index) {
+        return AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          margin: const EdgeInsets.symmetric(horizontal: 4),
+          width: currentItem == index ? 12 : 8,
+          height: currentItem == index ? 12 : 8,
+          decoration: BoxDecoration(
+            color: currentItem == index ? selectedColor : unselectedColor,
+            shape: BoxShape.circle,
+          ),
+        );
+      }),
+    );
+  }
+}
+
+
