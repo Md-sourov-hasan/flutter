b5f5ebf34ca3e2f9e2e90d2188a265ee52aaab6c
Md.sourov-hasan <mdsourovhasan2552@gmail.com>
2025-09-12 22:14:48 +0600

    english 104

---
+++ lib/expenses_list.dart
@@ -0,0 +1,17 @@
+  import 'package:first_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesLis extends StatelessWidget {
+  const ExpensesLis({super.key,
+  required this.expenses,
+  });
+  final List<Expense>expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: expenses.length,
+      itemBuilder: (context, index) => Text(expenses[index].title),
+    );
+  }
+}
