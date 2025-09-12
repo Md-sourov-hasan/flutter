3e842e463dc759720e0ef4371b230ffcc81d0eb0
Md.sourov-hasan <mdsourovhasan2552@gmail.com>
2025-09-12 21:49:44 +0600

    english 103

---
+++ lib/expenses.dart
@@ -0,0 +1,37 @@
+import 'package:first_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+   return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense>_registerdExpenses = [
+    Expense(title: 'FLUTTER COURSE',
+     amount: 19.99,
+      date: DateTime.now(),
+       category: Category.leisure,
+       ),
+           Expense(title: 'demon slayer',
+     amount: 20.00,
+      date: DateTime.now(),
+       category: Category.work,
+       ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+   return Scaffold(
+    body: Column(
+      children: [
+        Text('The chart'),
+        Text('expenses')
+      ],
+    ),
+   );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,10 @@
 // No Money / No Cash I am The Fan of Yellow Flash
-import 'package:first_app/login/login.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+import 'package:first_app/expenses.dart';
+void main(){
+  runApp(
+    MaterialApp(
+      home: Expenses(),
+    )
   );
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const LoginScreen(), // সবসময় LoginScreen দিয়ে শুরু
-    );
-  }
 }
