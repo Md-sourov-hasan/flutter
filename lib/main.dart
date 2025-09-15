6bbfaff465d22c94fb3a43c75ccca9b40c2d1e64
Md.sourov-hasan <mdsourovhasan2552@gmail.com>
2025-09-15 15:15:43 +0600

    fdfdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:first_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
-var kColorScheme= ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: kColorScheme,
         useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme(
+          backgroundColor: kColorScheme.primaryContainer, // শুধু background
+          foregroundColor: Colors.white, // সবসময় সাদা title + icon
+          surfaceTintColor: Colors.transparent, // overlay বন্ধ
+        ),
       ),
       home: Expenses(),
     ),
