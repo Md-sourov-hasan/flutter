a05ec14ee05a798a3cf715736c9601aab8dcc785
Md.sourov-hasan <mdsourovhasan2552@gmail.com>
2025-09-15 14:35:08 +0600

    english 126 half

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:first_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+var kColorScheme= ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: kColorScheme,
         useMaterial3: true,
       ),
       home: Expenses(),
