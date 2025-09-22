287d7bea3b6707c87e5306334d547ba414ac6580
Md-sourov-hasan <mdsourovhasan2552@gmail.com>
2025-09-22 15:27:50 +0600

    english 138

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -67,8 +67,7 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
-    MediaQuery.of(context).size.width;
-    MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     Widget mainContent = const Center(
       child: Text('No expense found. Start adding some!'),
     );
@@ -90,12 +89,19 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent),
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent),
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(child: mainContent),
+              ],
+            ),
     );
   }
 }
